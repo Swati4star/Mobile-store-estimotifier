10c64d2e105bdf4e4d7907bfc39f10790ac1f898
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-02-06 15:42:51 +0530

    Delete connection.inc.php

--- server-side/inc/connection.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$connect_error = 'Could not connect';
-	$mysql_host = 'localhost';
-	$mysql_user = 'csinseew_pg_69';
-	$mysql_pass = 'aloogobhi123';
-	$mysql_data = 'csinseew_pg_test';
-	
-	if(!@$connection = mysqli_connect($mysql_host , $mysql_user , $mysql_pass ,$mysql_data))
-		die($connect_error);
